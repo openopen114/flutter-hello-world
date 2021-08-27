098d647f0b5af620ab07a392e2ff8da9210f9cd2
openopen114 <openopen114@gmail.com>
2021-08-27 21:14:22 +0800

    hello world

---
+++ android/app/src/main/kotlin/com/openopen/demo_hello_text/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.openopen.demo_hello_text
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MyTitle'),
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('hello world'),
+    );
+  }
+}
